72b879578ac6c363c0915b7a83d3edf17d19bdf6
Bavly Wagih <bavlywagih696@gmail.com>
2026-08-14 17:59:25 +0300

    Task 3 - Components

---
+++ components/Content.tsx
@@ -0,0 +1,9 @@
+function Content() {
+    return (
+        <h2>
+            I am currently learning Components
+        </h2>
+    )
+}
+
+export default Content ;
---
+++ components/Profile.tsx
@@ -0,0 +1,11 @@
+function Profile () {
+    return (
+        <h2>
+            My name is Bavly <br />
+            I am learning React
+        </h2>
+    )
+}
+
+
+export default Profile;
